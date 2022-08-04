7deda4fc8cfae022b7a5748bae92fa7dfbd85b48
Daniel Kamar <cody@mccodepants.com>
2022-08-04 00:28:04 +0000

    feat(ward): display from backend

--- pages/ward.vue
+++ pages/ward.vue
@@ -1,50 +1,93 @@
 <template>
-  <div>
-  <h3 class="title grey--text pa-6">Ward</h3>
 
-  <div class="text-center">
-      <v-progress-circular
-        :rotate="360"
-        :size="100"
-        :width="15"
-        :value="value"
-        color="teal"
-      >
-        {{ value }}
-      </v-progress-circular>
+    <v-container grid-list-xs fluid>
 
-      <v-progress-circular
-        :rotate="-90"
-        :size="100"
-        :width="15"
-        :value="value"
-        color="primary"
-      >
-        {{ value }}
-      </v-progress-circular>
+      <v-row>
+        <v-col>
+          <v-layout wrap>
+            <h3 class="title grey--text">Ward</h3>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-layout>
+        </v-col>
+      </v-row>
 
-      <v-progress-circular
-        :rotate="90"
-        :size="100"
-        :width="15"
-        :value="value"
-        color="red"
-      >
-        {{ value }}
-      </v-progress-circular>
+      <div class="text-center">
+          <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="teal"
+          >
+            {{ value }}
+          </v-progress-circular>
 
-      <v-progress-circular
-        :rotate="180"
-        :size="100"
-        :width="15"
-        :value="value"
-        color="pink"
-      >
-        {{ value }}
-      </v-progress-circular>
-    </div>
-  </div>
+          <v-progress-circular
+            :rotate="-90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="primary"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="red"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="180"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="pink"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+      </div>
+
+      <v-row>
+        <template v-for="(item, index) in data">
+
+          <v-col>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <card-box
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  :title="item['CA-WardName']"
+                  :fileCount="item['ConstituencyCode'].toString()"
+                  fileSize="Wards"
+                  color="grey darken-4"
+                  flat
+                  iconColor="pink"
+                  titleClass="pink--text"
+                ></card-box>
+
+              </template>
+              <!-- <span>After 50% of Registered Votes have been cast</span> -->
+            </v-tooltip>
+
+          </v-col>
+        </template>
+      </v-row>
+
+    </v-container>
+
 </template>
+
 
 <script>
   export default {
@@ -52,7 +95,14 @@
       return {
         interval: {},
         value: 0,
+        data: null,
+        ConstituencyCode: 143,
       }
+    },
+    created() {
+
+      this.getWards();
+
     },
     beforeDestroy () {
       clearInterval(this.interval)
@@ -65,11 +115,33 @@
         this.value += 10
       }, 1000)
     },
-  }
+    methods: {
+      async getWards() {
+        const { data, error } = await this.$supabase
+          .from('ward')
+          // .select()
+          .select(`
+            CA-WardName,
+            ConstituencyCode,
+            ConstituencyName
+          `)
+          .eq('ConstituencyCode', this.ConstituencyCode)
+
+        this.data = data  
+        console.log(data)
+      },
+    },
+  };
 </script>
 
 <style scoped>
   .v-progress-circular {
     margin: 1rem;
   }
+  .v-card {
+    /* min-width: 48px;
+    max-width: 168px; */
+    width: 200px;
+  }
+
 </style>
