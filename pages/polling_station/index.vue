6a3965a30c5dac63423ad62a3b407c909d3e4133
Daniel Kamar <cody@mccodepants.com>
2022-08-04 17:54:51 +0000

    refactor(frontend): create slug routes

---
+++ pages/polling_station/index.vue
@@ -0,0 +1,85 @@
+<template>
+
+    <v-container grid-list-xs fluid>
+
+      <v-row>
+        <v-col>
+          <v-layout wrap>
+            <h3 class="title grey--text">Polling Station</h3>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-layout>
+        </v-col>
+      </v-row>
+
+
+      <v-row>
+        <template v-for="(item, index) in data">
+
+          <v-col>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <card-box
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  :title="item['PollingStationName']"
+                  :fileCount="item['ConstituencyCode'].toString()"
+                  fileSize="Wards"
+                  color="grey darken-4"
+                  flat
+                  iconColor="indigo"
+                  titleClass="indigo--text"
+                ></card-box>
+
+              </template>
+              <!-- <span>After 50% of Registered Votes have been cast</span> -->
+            </v-tooltip>
+
+          </v-col>
+        </template>
+      </v-row>
+
+    </v-container>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        data: null,
+        CA_WardCode: 718,
+      }
+    },
+    created() {
+
+      this.getWards();
+
+    },
+
+    methods: {
+      async getWards() {
+        const { data, error } = await this.$supabase
+          .from('polling_station_unique')
+          .select()
+          .eq('CA-WardCode', this.CA_WardCode)
+
+        this.data = data  
+        // console.log(data)
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .v-progress-circular {
+    margin: 1rem;
+  }
+  /* .v-card {
+    width: 400px;
+  } */
+
+</style>
---
+++ pages/ward/index.vue
@@ -0,0 +1,146 @@
+<template>
+
+    <v-container grid-list-xs fluid>
+
+      <v-row>
+        <v-col>
+          <v-layout wrap>
+            <h3 class="title grey--text">Ward</h3>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+          </v-layout>
+        </v-col>
+      </v-row>
+
+      <div class="text-center">
+          <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="teal"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="-90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="primary"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="90"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="red"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+          <v-progress-circular
+            :rotate="180"
+            :size="100"
+            :width="15"
+            :value="value"
+            color="pink"
+          >
+            {{ value }}
+          </v-progress-circular>
+
+      </div>
+
+      <v-row>
+        <template v-for="(item, index) in data">
+
+          <v-col>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <card-box
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                  :title="item['CA-WardName']"
+                  :fileCount="item['ConstituencyCode'].toString()"
+                  fileSize="Wards"
+                  color="grey darken-4"
+                  flat
+                  iconColor="indigo"
+                  titleClass="indigo--text"
+                ></card-box>
+
+              </template>
+              <!-- <span>After 50% of Registered Votes have been cast</span> -->
+            </v-tooltip>
+
+          </v-col>
+        </template>
+      </v-row>
+
+    </v-container>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        interval: {},
+        value: 0,
+        data: null,
+        ConstituencyCode: 143,
+      }
+    },
+    created() {
+
+      this.getWards();
+
+    },
+    beforeDestroy () {
+      clearInterval(this.interval)
+    },
+    mounted () {
+      this.interval = setInterval(() => {
+        if (this.value === 100) {
+          return (this.value = 0)
+        }
+        this.value += 10
+      }, 1000)
+    },
+    methods: {
+      async getWards() {
+        const { data, error } = await this.$supabase
+          .from('ward')
+          // .select()
+          .select(`
+            CA-WardName,
+            ConstituencyCode,
+            ConstituencyName
+          `)
+          .eq('ConstituencyCode', this.ConstituencyCode)
+
+        this.data = data  
+        // console.log(data)
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .v-progress-circular {
+    margin: 1rem;
+  }
+  .v-card {
+    /* min-width: 48px;
+    max-width: 168px; */
+    width: 200px;
+  }
+
+</style>
