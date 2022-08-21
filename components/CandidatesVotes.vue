6f4c4438c8b7fa3d6643c9dd3b03e868fd844437
Daniel Kamar <cody@mccodepants.com>
2022-08-21 23:39:39 +0000

    refactor(styling): position button svgs

--- components/CandidatesVotes.vue
+++ components/CandidatesVotes.vue
@@ -5,14 +5,14 @@
                 <div class="form-group">
                     <label class="text-gray-600 font-semibold text-lg">Candidates Names and Votes Cast</label>
                     <div v-for="(input, index) in candidates" :key="`nameInput-${index}`"
-                        class="input wrapper flex items-center">
-                        <input v-model="input.name" type="text" class="h-10 rounded-lg outline-none p-2"
+                        class="input wrapper d-flex items-center mb-2">
+                        <v-text-field v-model="input.name" outline type="text" class="h-10 rounded-lg p-2 mr-7"
                             placeholder=" Enter Candidate Name" />
-                        <input v-model="input.votesCast" type="number" class="h-10 rounded-lg outline-none p-2"
+                        <v-text-field v-model="input.votesCast" outline type="number" class="h-10 rounded-lg p-2"
                             placeholder=" Enter Votes cast" />
                         <!--          Add Svg Icon-->
-                        <svg @click="addField(input, candidates)" xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24" width="24" height="24" class="ml-2 cursor-pointer">
+                        <svg @click="addField(input, candidates)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            width="24" height="24" class="ml-2 mt-4 cursor-pointer">
                             <path fill="none" d="M0 0h24v24H0z" />
                             <path fill="green"
                                 d="M11 11V7h2v4h4v2h-4v4h-2v-4H7v-2h4zm1 11C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z" />
@@ -21,7 +21,7 @@
                         <!--          Remove Svg Icon-->
                         <svg v-show="candidates.length > 1" @click="removeField(index, candidates)"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                            class="ml-2 cursor-pointer">
+                            class="ml-2 mt-4 cursor-pointer">
                             <path fill="none" d="M0 0h24v24H0z" />
                             <path fill="#EC4899"
                                 d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z" />
@@ -43,10 +43,10 @@ export default {
     },
     methods: {
         addField(value, fieldType) {
-            fieldType.push({ 
+            fieldType.push({
                 value: "",
                 votesCast: null
-                });
+            });
         },
         removeField(index, fieldType) {
             fieldType.splice(index, 1);
@@ -56,4 +56,7 @@ export default {
 </script>
 
 <style lang="css">
+.v-text-field--outlined {
+    color: red !important;
+}
 </style>
